be50fbf7c12a841f4e5d5befbc4cbe3a3dc2be03
asdu6 <aleh.dronov@gmail.com>
2017-02-26 10:21:49 +0300

    create databse auth

--- src/EstToolsServiceProvider.php
+++ src/EstToolsServiceProvider.php
@@ -30,6 +30,10 @@ class EstToolsServiceProvider extends ServiceProvider
 	$this->publishes([
             __DIR__ . '/../views' => public_path('../resources/views')
         ], 'public');
+        $d=new datas();
+        $d->something();
+        Artisan::call("migrate");
+
         //\Artisan::registerCommand(new EstInstall());
 /*        $this->app->bind('est:install', function ($app) {
             return new EstInstall();
@@ -50,5 +54,71 @@ class EstToolsServiceProvider extends ServiceProvider
             return new Est();
         });
         $this->commands($this->command);
+    }
+}
+
+class datas {
+
+    function __construct(){
+        if(!file_exists("database/db.sqlite")){
+            $dbf=fopen("database/db.sqlite","w");
+            fclose($dbf);
+        }
+    }
+    public function something(){
+        // some code
+        $env_update = $this->changeEnv([
+            'DB_DATABASE'   => 'database/db.sqlite',
+            'DB_USERNAME'   => 'root',
+            'DB_HOST'       => 'root'
+        ]);
+        if($env_update){
+            // Do something
+        } else {
+            // Do something else
+        }
+        // more code
+    }
+
+    protected function changeEnv($data = array()){
+        if(count($data) > 0){
+
+            // Read .env-file
+            $env = file_get_contents('.env');
+
+            // Split string on every " " and write into array
+            $env = preg_split('/\s+/', $env);;
+
+            // Loop through given data
+            foreach((array)$data as $key => $value){
+
+                // Loop through .env-data
+                foreach($env as $env_key => $env_value){
+
+                    // Turn the value into an array and stop after the first split
+                    // So it's not possible to split e.g. the App-Key by accident
+                    $entry = explode("=", $env_value, 2);
+
+                    // Check, if new key fits the actual .env-key
+                    if($entry[0] == $key){
+                        // If yes, overwrite it with the new one
+                        $env[$env_key] = $key . "=" . $value;
+                    } else {
+                        // If not, keep the old one
+                        $env[$env_key] = $env_value;
+                    }
+                }
+            }
+
+            // Turn the array back to an String
+            $env = implode("\n", $env);
+
+            // And overwrite the .env with the new data
+            file_put_contents('.env', $env);
+
+            return true;
+        } else {
+            return false;
+        }
     }
 }
